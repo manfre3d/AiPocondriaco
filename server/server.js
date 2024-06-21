41fa647469d0d9710884e282d4b8e7294c281dad
Manfredi Piraino <manfredipiraino@gmail.com>
2024-06-21 21:59:42 +0200

    add: working gpt post call for tests + comments

--- server/server.js
+++ server/server.js
@@ -1,82 +1,81 @@
 const express = require("express");
 require("dotenv").config();
-const {OpenAI} = require("openai");
+const { OpenAI } = require("openai");
 const app = express()
+// import OpenAI from "openai";
 
 
 app.use(logger);
 app.use(express.json());
 
 
+//check if a .env file is properly configured and a apikey is correctly configured
+if (!process.env.OPENAI_API_KEY) {
+    console.error("OPENAI_API_KEY is not set. Check your .env file.");
+    process.exit(1); // Exit the application if the API key is not set
+}
+
+//set openai class configuring the api key in the class 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY
 });
-app.get("/", async (req,res)=>{
+
+//basic route test
+app.get("/", async (req, res) => {
 
     try {
-        return res.status(200).json({message:"server working"});
-        
+        return res.status(200).json({ message: "server working" });
+
     } catch (error) {
-        console.log(error);     
+        console.log(error);
     }
 
 });
 
-app.get("/fing-complexity", async (req, res) => {
+//Basic post test with chat gpt
+app.post("/chat-test", async (req, res) => {
     try {
-        const response = await openai.completions.create({
-            model: "text-davinci-003",
-            prompt: `
-                const example = (arr) => {
-                    arr.map((item)=>{
-                        console.log(item2);
-                    })
-                }
-            
-                The time complexity of this function is
-                ###
-            `,
-            max_tokens: 64,
-            temperature: 0,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0, 
-            stop: ["\n"],
+        const response = await openai.chat.completions.create({
+            messages: [{ role: "system", content: "What day is it?" }],
+            model: "gpt-3.5-turbo",
         });
+        console.log(response)
+        console.log(response.choices[0].message)
         return res.status(200).json({
             success: true,
-            data: response.data.choices[0].text
-        });
+            data:response.choices[0].message
+        })
     } catch (error) {
+        console.log(error);
         return res.status(400).json({
             success: false,
-            error: error.response? error.response.data: "there was an issue on the server"
+            error: error.response ? error.response.data : "there was an issue on the server"
         })
     }
 });
 
 
-//server port
+//server port 
+//todo add .env Port variable
 const port = process.env.PORT || 3000;
 
-//routes
-const promptsRouter = require("./routes/prompts.js");
+//routes sectiong to differentiate api calls
+const promptsRouter =require("./routes/prompts.js");
 
 
-//route use
-app.use("/prompts",promptsRouter)
+/*  route use
+    necessary to use the operations in the specific routes
+*/ 
+app.use("/prompts", promptsRouter)
 
-//basic logger
-function logger(req,res, next){
+/*basic logger that shows the url being called on the server*/
+function logger(req, res, next) {
     console.log(req.originalUrl);
     next();
 }
 
-
-app.listen(port,()=>{
-    console.log(openai)
+// server listening start
+app.listen(port, () => {
+    // console.log(openai)
     console.log(`Server listening on port ${port} ${process.env.OPENAI_API_KEY}`)
 });
-
-
-
