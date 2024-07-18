a97de3e9994ea9c3a21a45ea00aa0793913127ad
Manfredi Piraino <m.piraino@reply.it>
2024-07-18 23:52:19 +0200

    add: basic components routing initializzation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { "path": "", "component": LoginComponent },
+    { "path": "login", "component": LoginComponent },
+    { "path": "homepage", "component": HomepageComponent },
+    { 'path': 'error', component: ErrorPageComponent }
+  ];
+
