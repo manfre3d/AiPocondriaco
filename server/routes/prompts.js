2ca06f44d8d980c32768ade78d55b06ff9dbba04
Manfredi Piraino <manfredipiraino@gmail.com>
2024-07-24 00:06:00 +0200

    add: prompt interaction conversation storage for session

--- server/routes/prompts.js
+++ server/routes/prompts.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router()
 const openai =require("../server.js");
 
+let message = {
+    "role":"user", 
+    "content": "This is the beginning of your chat with AI. [To exit, send \"###\".]\n\nYou:"
+};
+
+let conversation = [];
+conversation.push(message)
+// completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation) 
+// message["content"] = "Assistant: {completion.choices[0].message.content} \nYou:"
+// conversation.push(completion.choices[0].message)
 
 
 router.get("/",(req,res)=>{
@@ -12,12 +22,23 @@ router.get("/",(req,res)=>{
 router.post("/test", async (req, res) => {
     try {
         console.log(req.body);
+        conversation.push(({
+            "role":"user",
+            "content":req.body.messagePrompt
+        }))
+        console.log(conversation);
         const response = await openai.chat.completions.create({
-            messages: [{ role: "system", content: req.body.messagePrompt }],
+            // messages: [{ role: "system", content: req.body.messagePrompt }],
+            messages: conversation,
             model: "gpt-3.5-turbo",
         });
+        message["content"] = `Assistant: ${response.choices[0].message.content} \nYou:`
+
+        console.log("RESPONSE")
         console.log(response)
         console.log(response.choices[0].message)
+        conversation.push(response.choices[0].message)
+
         return res.status(200).json({
             success: true,
             data:response.choices[0].message
